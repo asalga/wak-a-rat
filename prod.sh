7cb5194ff0e55d227fcd0b624cf057c32a1dc667
Andor Saga <andor.saga@gmail.com>
2018-09-17 19:21:17 -0400

    making prod build.

--- prod.sh
+++ prod.sh
@@ -1,6 +1,12 @@
+#!/bin/sh
+# production build shell script to save keystrokes!
+
+git add . && \
+git commit -m'making prod build.' && \
 git co gh-pages && \
 git merge master && \
 grunt prod && \
 git add . && \
 git commit -m"prod build" && \
-git push
+git push && \
+git co master
